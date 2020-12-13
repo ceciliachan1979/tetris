b71ba94ba7a22b36716a68067fb540e7a9f9cc70
ceciliachan1979 <75919064+ceciliachan1979@users.noreply.github.com>
2020-12-13 10:46:08 -0800

    Learning about the programming environment.

--- main.ts
+++ main.ts
@@ -1,1 +1,55 @@
+// https://arcade.makecode.com/reference
+// https://tetris.com/play-tetris
 
+scene.setBackgroundColor(1);
+
+// 1 is white
+// 2 is red
+// 3 is pink
+// 4 is orange
+// 5 is yellow
+// 6 is green
+// 7 is light green
+// 8 is dark blue
+// 9 is cyan
+// a is purple
+// b is pale
+// c is dark purple
+// d is weird   :(
+// e is dark red
+// f is black
+
+// A square does look like a square
+let mySprite  = sprites.create(img`
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+`)
+
+// The screen is 160 pixels wide and 120 pixels tall
+// The position of the sprite is in the middle of the sprite
+// 5 to 155 for x
+// 5 to 115 for y
+let x: number = 80;
+mySprite.setPosition(80, 60)
+game.onUpdateInterval(50, update);
+
+function update() {
+    // This speed seems okay
+    if (controller.left.isPressed()) {
+        x = x - 5;
+        mySprite.setPosition(x, 60);
+    }
+    else
+    {
+        x = 80;
+        mySprite.setPosition(80, 60)
+    }
+}
